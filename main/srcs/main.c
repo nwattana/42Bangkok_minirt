b5841802cd3aeb7597b942a8ef338b296e75615f
narin <narin.w@lexnetix.com>
2023-09-23 16:09:54 +0700

    Save

---
+++ main/srcs/main.c
@@ -0,0 +1,39 @@
+#include "../inc/minirt.h"
+
+void debug_mlx_status(t_mlx_confix *vars)
+{
+	printf("is_initmlx: %d\n", vars->is_initmlx);
+	printf("is_initwin: %d\n", vars->is_initwin);
+	printf("is_init_img: %d\n", vars->is_init_img);
+}
+
+void error_exit(char *msg)
+{
+	printf("%s", msg);
+	exit(1);
+}
+
+int validate_args(int argc, char **argv)
+{
+	int i;
+
+	i = 0;
+	if (argc < 2)
+		error_exit("Error\nNo arguments\n");
+	if (argc != 2)
+		error_exit("Error\nToo many arguments\n");
+	
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	t_prog	program;
+
+	// FUNC
+	validate_args(argc, argv, prog);
+
+
+
+	return (0);
+}
