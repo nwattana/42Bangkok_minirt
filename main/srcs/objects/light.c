f7b63d92589c558cc35d0287b11a0ee37cea77c5
narin <narin.w@lexnetix.com>
2023-10-05 00:21:52 +0700

    Clean do other try

--- main/srcs/objects/light.c
+++ main/srcs/objects/light.c
@@ -20,30 +20,3 @@ void    clean_light(void *lg)
     light = (t_light *)lg;
     free(light);
 }
-
-int point_light(t_light *light, t_list *current_obj, t_interparam *param, t_color *color, double *intensity)
-{
-    t_vec3d     light_dir;
-    t_point3d   light_start;
-
-    double angle;
-
-    vec3d_minus(&light_dir, &light->position, &param->intersection_point);
-    vec3d_normalize(&light_dir);
-
-    vec3d_assign(&light_start, &param->intersection_point);
-    angle = acos(vec3d_dot(&param->local_normal, &light_dir));
-    
-    if (angle > 1.5708)
-    {
-        color_copy(color, &light->color);
-        *intensity = 0.0;
-        return (0);
-    }
-    else
-    {
-        color_copy(color, &light->color);
-        *intensity = light->intensity * (1.0 - (angle / 1.5708));
-        return (1);
-    }
-}
